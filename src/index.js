204782d2353c7066961bfa8766cffdb22cdeb33c
jobir307 <jobirrrrr@gmail.com>
2024-01-19 08:21:48 +0500

    done

--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const txt = 'Hello, sammi'
+
+const el = (
+  <div>
+    <h1 className='greeting'>{['val1', 'val2']}</h1>
+    <input type='text' />
+    <button>click</button>
+  </div>
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render(el);
+
+/* root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+); */
